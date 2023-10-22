2643f770bb3e0490524c2c1e2025b413a96e36d9
mikoonezaa <mikoo120422@gmail.com>
2023-10-22 13:09:10 +0700

    nothing

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,7 +1,7 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'localhost' ,
+  host: 'localhost',
   port: 3306, //port default 3306 lihat xampp
   username: 'root', // username default xampp root
   password: '', // password default xampp string kosong
