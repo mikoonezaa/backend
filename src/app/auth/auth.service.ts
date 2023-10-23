ca97d689a26587c2f6eed62a3bc7a3b91bef8432
mikoonezaa <mikoo120422@gmail.com>
2023-10-23 12:45:31 +0700

    update

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './auth.entity';
+import { Repository } from 'typeorm';
+import { JwtService } from '@nestjs/jwt/dist';
+import BaseResponse from 'src/utils/dto/response/base.response';
 
 @Injectable()
-export class AuthService {}
+export class AuthService extends BaseResponse {
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private jwtService: JwtService, //panggil kelas jwt service
+  ) {
+    super();
+  }
+}
---
+++ src/config/jwt.config.ts
@@ -0,0 +1,5 @@
+export const jwt_config = {
+  acces_token_secret: 'belajar jwt',
+  expired: 3600,
+  refesh_token_secret: 'fajfngjgan',
+};
