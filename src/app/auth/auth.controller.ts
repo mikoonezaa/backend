835149426b72ef688e8833a2dba295c37938e329
mikoonezaa <mikoo120422@gmail.com>
2023-10-23 13:22:01 +0700

    update

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { RegisterDto } from './auth.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+  @Post('login')
+  async register(@Body() payload: RegisterDto) {
+    return this.authService.login(payload);
+  }
+}
