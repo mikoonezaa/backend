021f877dbde314144b10756aa5d7a7db67e8f900
mikoonezaa <mikoo120422@gmail.com>
2023-12-13 08:05:13 +0700

    delete

--- src/app/auth/auth.interface.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-interface jwtPayload {
-  id: number;
-  nama: string;
-  email: string;
-}
--- src/config/jwt.config.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable prettier/prettier */
-export const jwt_config = {
-  acces_token_secret: 'belajar jwt',
-  expired: 3600,
-  refesh_token_secret: 'fajfngjgan',
-};
