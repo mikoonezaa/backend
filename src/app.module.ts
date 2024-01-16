a5b6f630385d39f99148449054fbe4107b53dc32
mikoonezaa <mikoo120422@gmail.com>
2024-01-16 10:10:58 +0700

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { AuthModule } from './app/auth/auth.module';
 import { MailModule } from './app/mail/mail.module';
+import { KategoriModule } from './app/kategori/kategori.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), AuthModule, MailModule],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
+    MailModule,
+    KategoriModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
